60e97b7ff6fbd07235741dda791b0388783b3c13
jstrickler <jstrickler@gmail.com>
2021-06-18 13:45:08 -0400

    x

--- pastable.py
+++ pastable.py
@@ -5,12 +5,12 @@
 fruits = ["pomegranate", "cherry", "apricot", "apple",
 "lemon", "kiwi", "orange", "lime", "watermelon", "guava",
 "papaya", "fig", "pear", "banana", "tamarind", "persimmon",
-"elderberry", "peach", "blueberry", "lychee", "grape", "date" ]
+"elderberry", "peach", "blueberry", "lychee", "grape", "date"]
 
 fruits = ["pomegranate", "cherry", "apricot", "Apple",
 "lemon", "Kiwi", "ORANGE", "lime", "Watermelon", "guava",
 "Papaya", "FIG", "pear", "banana", "Tamarind", "Persimmon",
-"elderberry", "peach", "BLUEberry", "lychee", "GRAPE", "date" ]
+"elderberry", "peach", "BLUEberry", "lychee", "GRAPE", "date"]
 
 nums = [800,80,1000,32,255,400,5,5000]
 
@@ -30,17 +30,25 @@ people = [
 
 airports = {
     'EWR': 'Newark',
+    'YYZ': 'Toronto',
     'MCI': 'Kansas City',
     'SFO': 'San Francisco',
     'RDU': 'Raleigh-Durham',
     'SJC': 'San Jose',
     'MCO': 'Orlando',
+    'YCC': 'Calgary',
     'ABQ': 'Albuquerque',
     'OAK': 'Oakland',
-    'SAC': 'Sacramento',
+    'SMF': 'Sacramento',
+    'YOW': 'Ottawa',
     'IAD': 'Dulles',
 }
+
+
 
 colors = ['red', 'green', 'purple', 'orange', 'brown',
           'black', 'olive', 'navy', 'white', 'black',
           'pink', 'chartreuse']
+
+
+
